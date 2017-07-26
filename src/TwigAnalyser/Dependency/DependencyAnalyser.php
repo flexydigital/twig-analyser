e0ebaea3856fb4bd55170b3a668e511a47ae5b3d
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2017-07-26 16:08:04 -0300

    fixed sintax error

--- src/TwigAnalyser/Dependency/DependencyAnalyser.php
+++ src/TwigAnalyser/Dependency/DependencyAnalyser.php
@@ -27,11 +27,11 @@ class DependencyAnalyser
 
     /**
      * @param Tokenizer $tokenizer
-     * @param Dependency $dependencyFactory
+     * @param DependencyFactory $dependencyFactory
      */
     public function __construct(
         Tokenizer $tokenizer,
-        Dependency $dependencyFactory
+        DependencyFactory $dependencyFactory
     ) {
         $this->tokenizer = $tokenizer;
         $this->dependencyFactory = $dependencyFactory;
@@ -73,3 +73,4 @@ class DependencyAnalyser
         return $relations;
     }
 }
+
